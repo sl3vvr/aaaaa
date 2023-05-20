92666e57c097b048ffa2f06b9783cf035974070b
sl3vvr <134112783+sl3vvr@users.noreply.github.com>
2023-05-19 19:55:09 -0500

    Delete gif.js

--- assets/js/gif.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-    var gif = document.getElementById("gif");
-    var shopContainer = document.getElementById("shopContainer");
-
-    gif.addEventListener("animationend", function() {
-        gif.style.opacity = 0;
-        shopContainer.style.opacity = 1;
-    });
-});
